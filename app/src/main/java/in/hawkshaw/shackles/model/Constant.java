d8a63507e1bcb3382eab4b1a0fbbf4547e5d4e64
siva-gk <g.siva97@gmail.com>
2019-05-24 00:35:16 +0530

    Added Instagram Login option. Have to fix the UI as well.

---
+++ app/src/main/java/in/hawkshaw/shackles/model/Constant.java
@@ -0,0 +1,8 @@
+package in.hawkshaw.shackles.model;
+
+public class Constant {
+    public static final String CLIENT_ID = "edb468e9c7164ebf9570e24f70f649ba";
+    public static final String CLIENT_SECRET = "5a0ec06f7de948f8ae47707c33ffef69";
+    public static final String REDIRECT_URI = "http://shackles.com";
+    public static final String INSTA_LOGIN_URL = "https://api.instagram.com/oauth/authorize/?client_id="+CLIENT_ID+"&redirect_uri="+REDIRECT_URI+"&response_type=code";
+}
